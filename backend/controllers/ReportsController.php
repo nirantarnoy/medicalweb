c2d46ca4736b92adbdc50c3638da7d3b6b255052
CICNETGRP\niran.w <niran_tarlek@hotmail.com>
2023-02-21 21:49:04 +0700

    'update'

---
+++ backend/controllers/ReportsController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace backend\controllers;
+
+use backend\models\Unit;
+use backend\models\UnitSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * UnitController implements the CRUD actions for Unit model.
+ */
+class ReportsController extends Controller
+{
+    public $enableCsrfValidation = false;
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all Unit models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $pageSize = \Yii::$app->request->post("perpage");
+
+        $searchModel = new UnitSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        $dataProvider->pagination->pageSize = $pageSize;
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'perpage' => $pageSize,
+        ]);
+    }
+
+    public function actionLowstock(){
+        return $this->render('_lowstock');
+    }
+
+
+}
