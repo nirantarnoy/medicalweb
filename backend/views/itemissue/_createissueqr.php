f0a194012ee63a3b542e26562b925b2a2ee2a97d
CICNETGRP\niran.w <niran_tarlek@hotmail.com>
2023-03-08 21:28:00 +0700

    'update'

--- backend/views/itemissue/_createissueqr.php
+++ backend/views/itemissue/_createissueqr.php
@@ -2,7 +2,8 @@
 ?>
 <div class="row">
     <div class="col-lg-4">
-        <?=$product_code;?>
+        <label for="">รหัสสินค้า</label>
+        <input type="text" class="form-control" value="<?=$product_code;?>" readonly>
     </div>
     <div class="col-lg-4"></div>
     <div class="col-lg-4"></div>
