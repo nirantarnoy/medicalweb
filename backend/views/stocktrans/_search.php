57a3aeca2163e4f42afa3c753c40ecff39c8fd53
CICNETGRP\niran.w <niran_tarlek@hotmail.com>
2023-04-27 20:48:51 +0700

    'update'

--- backend/views/stocktrans/_search.php
+++ backend/views/stocktrans/_search.php
@@ -41,6 +41,9 @@ use yii\widgets\ActiveForm;
                 'data' => \yii\helpers\ArrayHelper::map(\backend\helpers\StockType::asArrayObject(),'id','name'),
             ]) ?>
         </div>
+        <div class="col-lg-3">
+            <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        </div>
     </div>
 
 
@@ -51,9 +54,9 @@ use yii\widgets\ActiveForm;
 
 
 
-    <div class="form-group">
-        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-    </div>
+<!--    <div class="form-group">-->
+<!--        --><?//= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+<!--    </div>-->
 
     <?php ActiveForm::end(); ?>
 
